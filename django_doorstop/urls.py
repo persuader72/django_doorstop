0e7eb7dd5c50f6546027d7fb7305d61e3847d8c7
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2020-05-23 08:45:10 +0200

    Index redirect to app

--- django_doorstop/urls.py
+++ django_doorstop/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('r/', include('requirements.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='/r'), name='index')
 ]
